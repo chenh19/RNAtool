58ef67e5549cb402b6e366c83f820f14e216ea8d
Hang Chen <chen_h@outlook.com>
2023-02-14 02:06:47 -0600

    Update fastqc.sh

--- FastQC/fastqc.sh
+++ FastQC/fastqc.sh
@@ -6,4 +6,4 @@ sudo apt-get update && sudo apt-get install fastqc -y
 # run fastqc in parallel
 cd ~/Developing/
 [ ! -d ./1_HC_fastqc/ ] && mkdir ./1_HC_fastqc/
-fastqc --threads 16 ./0.PIE-seq_raw/FastQ/*.fastq.gz --outdir=./1_HC_fastqc/
+fastqc --threads 16 ./0_PIE-seq_raw/FastQ/*.fastq.gz --outdir=./1_HC_fastqc/
