55068d82374ec70254b05044bb6dbf79dddae30f
Hang Chen <chen_h@outlook.com>
2023-02-17 18:23:40 -0600

    Add files via upload

---
+++ JACUSA2/jacusa2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+## JACUSA2
+wget -q https://github.com/dieterich-lab/JACUSA2/releases/download/v2.0.2-RC/JACUSA_v2.0.2-RC.jar -O jacusa2.jar
+[ ! -d /opt/RNAtool/ ] && sudo mkdir /opt/RNAtool/
+sudo cp -f ./jacusa2.jar /opt/RNAtool/
+[ ! -f ~/.bashrc] ] && touch ~/.bashrc
+if ! grep -q "jacusa2='java -jar /opt/RNAtool/jacusa2.jar'" ~/.bashrc ; then echo -e "alias jacusa2='java -jar /opt/RNAtool/jacusa2.jar'" >> ~/.bashrc ; fi
+rm -f ./jacusa2.jar
+
+
+## JACUSA2helper
+wget https://bioconductor.org/packages/3.16/data/annotation/src/contrib/BSgenome.Hsapiens.NCBI.GRCh38_1.3.1000.tar.gz
+wget https://github.com/dieterich-lab/JACUSA2helper/releases/download/v1.99-9600/JACUSA2helper_1.9.9.9600.tar.gz
+echo -e "devtools::install_local('BSgenome.Hsapiens.NCBI.GRCh38_1.3.1000.tar.gz', force = TRUE)\ndevtools::install_local('JACUSA2helper_1.9.9.9600.tar.gz', dependencies = FALSE, build_vignettes = TRUE, force = TRUE)" > ./jacusa2helper.R
+sudo Rscript ./jacusa2helper.R
+rm -f ./BSgenome.Hsapiens.NCBI.GRCh38_1.3.1000.tar.gz ./JACUSA2helper_1.9.9.9600.tar.gz ./jacusa2helper.R
---
+++ Picard/picard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## PICARD
+wget -q https://github.com/broadinstitute/picard/releases/download/2.27.5/picard.jar -O picard.jar
+[ ! -d /opt/RNAtool/ ] && sudo mkdir /opt/RNAtool/
+sudo cp -f ./picard.jar /opt/RNAtool/
+[ ! -f ~/.bashrc] ] && touch ~/.bashrc
+if ! grep -q "picard='java -jar /opt/RNAtool/picard.jar'" ~/.bashrc ; then echo -e "alias picard='java -jar /opt/RNAtool/picard.jar'" >> ~/.bashrc ; fi
+rm -f ./picard.jar
