d23cbd37e7ee2f9fb18871ce2d1be8a1a74e8e03
Hang Chen <chen_h@outlook.com>
2023-02-17 18:31:18 -0600

    Update STAR.sh

--- STAR/STAR.sh
+++ STAR/STAR.sh
@@ -4,9 +4,9 @@
 sudo echo ""
 [ ! -d ./STAR.zip ] && wget -q https://github.com/alexdobin/STAR/releases/download/2.7.10b/STAR_2.7.10b.zip -O STAR.zip #_to_be_updated
 unzip -o -q STAR.zip && sleep 1 && rm -f STAR.zip && sleep 1
-[ ! -d /opt/STAR/ ] && sudo mkdir /opt/STAR/
-sudo cp -f ./STAR*/Linux_x86_64/* /opt/STAR/ && sleep 1
+[ ! -d /opt/RNAtool/ ] && sudo mkdir /opt/RNAtool/
+sudo cp -f ./STAR*/Linux_x86_64/* /opt/RNAtool/ && sleep 1
 rm -rf ./STAR*/
 [ ! -f ~/.bashrc] ] && touch ~/.bashrc
-if ! grep -q "alias STAR='/opt/STAR/STAR'" ~/.bashrc ; then echo -e "alias STAR='/opt/STAR/STAR'" >> ~/.bashrc ; fi
-if ! grep -q "alias STARlong='/opt/STAR/STARlong'" ~/.bashrc ; then echo -e "alias STARlong='/opt/STAR/STARlong'" >> ~/.bashrc ; fi
+if ! grep -q "alias STAR='/opt/RNAtool/STAR'" ~/.bashrc ; then echo -e "alias STAR='/opt/RNAtool/STAR'" >> ~/.bashrc ; fi
+if ! grep -q "alias STARlong='/opt/RNAtool/STARlong'" ~/.bashrc ; then echo -e "alias STARlong='/opt/RNAtool/STARlong'" >> ~/.bashrc ; fi
