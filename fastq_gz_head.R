dbaa8a96d2e8c126eec9f7bd57c952eee9d9d073
Hang Chen <chen_h@outlook.com>
2024-10-17 12:14:31 -0500

    Update fastq_gz_head.R

--- fastq_gz_head.R
+++ fastq_gz_head.R
@@ -1,2 +1,6 @@
+# Set the folder containing current R script as working directory
+setwd(".")
+print(paste("Current working dir:", getwd()))
+
 # check frist few lines in .fastq.gz files
 readLines(gzfile("file_path"), n = 50)
